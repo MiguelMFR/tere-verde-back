96f7885db80a28b776c2daa549a1aaf3835a4c96
MiguelMFR <guelferreirar@gmail.com>
2026-03-04 20:03:12 -0300

    refactor: rename field "nome" to "name" in RegisterDTO and remove unused methods

--- src/main/java/tere_verde/domain/dto/usuario/RegisterDTO.java
+++ src/main/java/tere_verde/domain/dto/usuario/RegisterDTO.java
@@ -1,26 +1,12 @@
 package tere_verde.domain.dto.usuario;
 
 import jakarta.validation.constraints.Email;
-import tere_verde.domain.entity.usuario.Usuario;
 
 public record RegisterDTO(
-    String nome,
+    String name,
     @Email
     String email,
     String senha
 ) {
-    public Usuario toEntity(){
-        Usuario usuario = new Usuario();
-        usuario.setNome(this.nome);
-        usuario.setEmail(this.email);
-        usuario.setSenha(this.senha);
-        return usuario;
-    }
-
-    public static RegisterDTO toDTO (Usuario usuario){
-        return new RegisterDTO(
-                usuario.getNome(),
-                usuario.getEmail(),
-                usuario.getSenha());
-    }
+        
 }
